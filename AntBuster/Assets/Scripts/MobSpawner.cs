880ecacf0f2c0f7776d032bfb91536ad5fe71910
DESKTOP-51TGSF5\KGA <dlwnsdud0612@gmail.com>
2023-07-20 21:34:55 +0900

    Style : .

---
+++ AntBuster/Assets/Scripts/MobSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    public GameObject[] mobPrefab = default;
+    private float speed = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
